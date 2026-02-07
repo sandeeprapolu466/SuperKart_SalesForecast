91299e7ad4bf192eb2865a8d15e94d3665a9a6a5
sandeeprapolu466 <sandeep.466.r@gmail.com>
2026-02-07 19:23:36 +0000

    Updated Hugging Space URL

--- week_4_mlops_superkart/hosting/hosting.py
+++ week_4_mlops_superkart/hosting/hosting.py
@@ -5,7 +5,7 @@ api = HfApi(token=os.getenv("HF_TOKEN"))
 
 api.upload_folder(
     folder_path="week_4_mlops_superkart/deployment",
-    repo_id="sandeep466/superkart-sales-forecast",
+    repo_id="sandeep466/SuperKart-SalesForecast",
     repo_type="space",
     path_in_repo="",
 )
