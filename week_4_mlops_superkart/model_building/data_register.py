3e736257597369415fdf2fc6753eef5f17fe827a
sandeeprapolu466 <sandeep.466.r@gmail.com>
2026-02-08 18:36:48 +0000

    Initial SuperKart MLOps pipeline

--- week_4_mlops_superkart/model_building/data_register.py
+++ week_4_mlops_superkart/model_building/data_register.py
@@ -2,14 +2,14 @@ from huggingface_hub.utils import RepositoryNotFoundError, HfHubHTTPError
 from huggingface_hub import HfApi, create_repo
 import os
 
-# Define Hugging Face dataset repository
+# Definin Hugging Face dataset repository
 repo_id = "sandeep466/superkart-sales-dataset"
 repo_type = "dataset"
 
 # Initialize API client
 api = HfApi(token=os.getenv("HF_TOKEN"))
 
-# Step 1: Check if the dataset repository exists
+#Checking if the dataset repository exists
 try:
     api.repo_info(repo_id=repo_id, repo_type=repo_type)
     print(f"Dataset '{repo_id}' already exists. Using it.")
